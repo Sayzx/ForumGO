657cd48e3cbc0098e2521c2967c4aa7972139cf0
Aylan <aylan.dehais@ynov.com>
2024-06-21 13:41:20 +0200

    [add] add showtopic front

--- internal/api/api.go
+++ internal/api/api.go
@@ -74,3 +74,60 @@ func GetAllTopics() []Topic {
 
 	return topics
 }
+
+func GetAllTopicsById(id int) []Topic {
+	// Connect to the SQLite 3 database
+	db, err := dbsql.ConnectDB() // Use the renamed import
+	if err != nil {
+		log.Println("Could not connect to the database:", err)
+		return nil
+	}
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Println("Could not close the database connection:", err)
+		}
+	}()
+
+	// Prepare the query to get all topics
+	stmt, err := db.Prepare("SELECT id, title, content, owner, avatar FROM topics where categoryid = ?")
+	if err != nil {
+		log.Println("Could not prepare query:", err)
+		return nil
+	}
+	defer func() {
+		if err := stmt.Close(); err != nil {
+			log.Println("Could not close the statement:", err)
+		}
+	}()
+
+	// Execute the query
+	rows, err := stmt.Query(id)
+	if err != nil {
+		log.Println("Could not execute query:", err)
+		return nil
+	}
+	defer func() {
+		if err := rows.Close(); err != nil {
+			log.Println("Could not close the rows:", err)
+		}
+	}()
+
+	// Process the result
+	var topics []Topic
+	for rows.Next() {
+		var topic Topic
+		err := rows.Scan(&topic.ID, &topic.Title, &topic.Content, &topic.Owner, &topic.Avatar)
+		if err != nil {
+			log.Println("Could not scan row:", err)
+			return nil
+		}
+		topics = append(topics, topic)
+	}
+
+	if err = rows.Err(); err != nil {
+		log.Println("Error encountered during row iteration:", err)
+		return nil
+	}
+
+	return topics
+}
--- internal/handler/showtopics.go
+++ internal/handler/showtopics.go
@@ -1,7 +1,14 @@
 package handler
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func ShowTopicsHandler(w http.ResponseWriter, r *http.Request) {
+	// Récupérer l'id dans le lien ( id := r.URL.Query().Get("id") )
+
+	categoryid := r.URL.Query().Get("id")
+	fmt.Println(categoryid)
 	http.ServeFile(w, r, "web/templates/showtopics.html")
 }
