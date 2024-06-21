1a60a5e3e136cf3d48896603217b92df89730d60
Aylan <aylan.dehais@ynov.com>
2024-06-21 14:42:19 +0200

    [add] fix css & add showtopic age

--- internal/handler/showtopics.go
+++ internal/handler/showtopics.go
@@ -1,14 +1,67 @@
 package handler
 
 import (
-	"fmt"
+	"html/template"
+	"log"
+	"main/internal/api"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 func ShowTopicsHandler(w http.ResponseWriter, r *http.Request) {
-	// Récupérer l'id dans le lien ( id := r.URL.Query().Get("id") )
-
+	// Récupérer l'id dans le lien
 	categoryid := r.URL.Query().Get("id")
-	fmt.Println(categoryid)
-	http.ServeFile(w, r, "web/templates/showtopics.html")
+
+	// Récupérer les topics par catégorie
+	topics := api.GetAllTopicsById(categoryid)
+	if topics == nil {
+		http.Error(w, "Could not fetch topics", http.StatusInternalServerError)
+		return
+	}
+
+	// Préparer les données pour le template
+	var data struct {
+		LoggedIn bool
+		Avatar   string
+		Topics   []api.Topic
+		Like     int
+		Dislike  int
+	}
+	data.Topics = topics
+
+	// Tentative de récupération du cookie utilisateur
+	cookie, err := r.Cookie("user")
+	if err == nil && cookie != nil {
+		value, err := url.QueryUnescape(cookie.Value)
+		if err != nil {
+			log.Println("Error unescaping cookie value:", err)
+			http.Error(w, "Error processing cookie", http.StatusBadRequest)
+			return
+		}
+
+		parts := strings.SplitN(value, ";", 2)
+		if len(parts) == 2 {
+			data.LoggedIn = true
+			data.Avatar = parts[1]
+		}
+	}
+
+	if !data.LoggedIn {
+		// Définir l'avatar par défaut si l'utilisateur n'est pas connecté
+		data.Avatar = "https://media.discordapp.net/attachments/1224092616426258432/1252742512209301544/1247.png"
+	}
+
+	// Charger et exécuter le template showtopics.html
+	tmpl, err := template.ParseFiles("web/templates/showtopics.html")
+	if err != nil {
+		log.Println("Error parsing template:", err)
+		http.Error(w, "Error parsing template", http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, data); err != nil {
+		log.Println("Error executing template:", err)
+		http.Error(w, "Error executing template", http.StatusInternalServerError)
+	}
 }
