3cb940563b96bd977768a308ee70b8ef35f177c5
gonicolas <nicolas.gouy@ynov.com>
2024-06-24 16:07:37 +0200

    [fix] Topics now display on home page !

--- internal/api/api.go
+++ internal/api/api.go
@@ -22,7 +22,7 @@ type Topic struct {
 	Like         int
 	Dislike      int
 	ContentShort string
-	createat     string
+	CreateAt     *string // Utilisation d'un pointeur pour gérer les valeurs NULL
 	Username     string
 }
 
@@ -38,8 +38,7 @@ func GetUsernameByCookie(r *http.Request) string {
 }
 
 func GetAllTopics() []Topic {
-	// Connect to the SQLite 3 database
-	db, err := dbsql.ConnectDB() // Use the renamed import
+	db, err := dbsql.ConnectDB()
 	if err != nil {
 		log.Println("Could not connect to the database:", err)
 		return nil
@@ -50,7 +49,6 @@ func GetAllTopics() []Topic {
 		}
 	}()
 
-	// Prepare the query to get all topics
 	stmt, err := db.Prepare("SELECT id, title, content, owner, avatar, createat FROM topics")
 	if err != nil {
 		log.Println("Could not prepare query:", err)
@@ -62,7 +60,6 @@ func GetAllTopics() []Topic {
 		}
 	}()
 
-	// Execute the query
 	rows, err := stmt.Query()
 	if err != nil {
 		log.Println("Could not execute query:", err)
@@ -74,11 +71,10 @@ func GetAllTopics() []Topic {
 		}
 	}()
 
-	// Process the result
 	var topics []Topic
 	for rows.Next() {
 		var topic Topic
-		err := rows.Scan(&topic.ID, &topic.Title, &topic.Content, &topic.Owner, &topic.Avatar, &topic.createat)
+		err := rows.Scan(&topic.ID, &topic.Title, &topic.Content, &topic.Owner, &topic.Avatar, &topic.CreateAt)
 		if err != nil {
 			log.Println("Could not scan row:", err)
 			return nil
@@ -95,8 +91,7 @@ func GetAllTopics() []Topic {
 }
 
 func GetAllTopicsById(id string) []Topic {
-	// Connect to the SQLite 3 database
-	db, err := dbsql.ConnectDB() // Use the renamed import
+	db, err := dbsql.ConnectDB()
 	if err != nil {
 		log.Println("Could not connect to the database:", err)
 		return nil
@@ -107,7 +102,6 @@ func GetAllTopicsById(id string) []Topic {
 		}
 	}()
 
-	// Prepare the query to get all topics
 	stmt, err := db.Prepare("SELECT id, title, content, owner, avatar, like, dislike FROM topics where categoryid = ?")
 	if err != nil {
 		log.Println("Could not prepare query:", err)
@@ -119,7 +113,6 @@ func GetAllTopicsById(id string) []Topic {
 		}
 	}()
 
-	// Execute the query
 	rows, err := stmt.Query(id)
 	if err != nil {
 		log.Println("Could not execute query:", err)
@@ -131,7 +124,6 @@ func GetAllTopicsById(id string) []Topic {
 		}
 	}()
 
-	// Process the result
 	var topics []Topic
 	for rows.Next() {
 		var topic Topic
@@ -140,7 +132,6 @@ func GetAllTopicsById(id string) []Topic {
 			log.Println("Could not scan row:", err)
 			return nil
 		}
-		// Compute ContentShort
 		if len(topic.Content) > 50 {
 			topic.ContentShort = topic.Content[:50] + "..."
 		} else {
